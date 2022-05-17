a1af80da1383a877656bae81db34f57ab1ca356e
Harshini-epam <102809826+Harshini-epam@users.noreply.github.com>
2022-05-17 14:41:01 +0530

    Updated

--- JS Tasks/Object array prototype/Using closures to share class state.js
+++ JS Tasks/Object array prototype/Using closures to share class state.js
@@ -1,33 +1,17 @@
 var Cat = (function () {
-    var instances = { count: 0, totalWeight: 0, averageWeight: 0 };
-  
-    function constructor(name, weight) {
-      if (!name || !weight) {
-        throw new Error("plz provide both name and weight");
+  const cats = []
+  const constructor = function(name,weight){
+    if (!name || !weight) throw Error('invalid parameters')
+    Object.defineProperty(this,'name', { get: () =>  name  }) //function() { return name }  
+    Object.defineProperty(this,'weight', { get: () => weight, set: value => weight = value })
+    cats.push(this)     
+  }
+  constructor.averageWeight = function() {
+      sum = 0
+      for(i in cats){
+        sum += cats[i].weight 
       }
-      instances.count++;
-      this.name = name;
-  
-      Object.defineProperty(this, "weight", {
-        get: function () {
-          return this.weight1 || 0;
-        },
-        set: function (val) {
-          instances.totalWeight += val - this.weight;
-          instances.averageWeight = instances.totalWeight / instances.count;
-          return (this.weight1 = val);
-        },
-      });
-  
-      this.weight = weight;
+      return sum/cats.length
     }
-    constructor.averageWeight = () => instances.averageWeight;
-    return constructor;
-  })();
-  
-  garfield = new Cat("garfield", 25);
-  Cat.averageWeight();
-  
-  felix = new Cat("felix", 15);
-  Cat.averageWeight();
-  
+  return constructor
+}());
