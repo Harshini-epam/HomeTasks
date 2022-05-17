b4e07b880df064c1a2cd0aae5d99ee2886c318f9
Harshini-epam <102809826+Harshini-epam@users.noreply.github.com>
2022-05-17 14:44:02 +0530

    Without splice()

--- JS Tasks/Scope and Functions/Partition On.js
+++ JS Tasks/Scope and Functions/Partition On.js
@@ -1,8 +1,26 @@
 function partitionOn(pred, items) {
-    var tarr = items.filter(ele => pred(ele));
-    var farr = items.filter(ele => !pred(ele));
-    items.splice(0);
-    items.push(...farr);
-    items.push(...tarr);
-    return farr.length;
-  }
+    var temp = [],temp1 = [];
+    items.forEach(element => {
+      if(!pred(element)){
+        temp.push(element);
+      }
+      else{
+        temp1.push(element);
+      }
+    });
+    for(var i = 0;i<temp.length;i++){
+      items[i] = temp[i];
+    }
+    for(var j = 0;j<temp1.length;j++){
+      items[i+j] = temp1[j];
+    }
+
+
+//function partitionOn(pred, items) {
+//var tarr = items.filter(ele => pred(ele));
+    //var farr = items.filter(ele => !pred(ele));
+    //items.splice(0);
+    //items.push(...farr);
+    //items.push(...tarr);
+    //return farr.length;
+ // }
