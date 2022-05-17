b4f02861c2cec4f12baea9672937739ad25b7447
Harshini-epam <102809826+Harshini-epam@users.noreply.github.com>
2022-05-17 14:39:20 +0530

    Updated

--- JS Tasks/Object array prototype/A Chain adding function.js
+++ JS Tasks/Object array prototype/A Chain adding function.js
@@ -1,8 +1,10 @@
-function add(n){
-    // Let the currying begin!
-     let val=function(a){
-       return add(n+a);
-     }
-     val.valueOf=()=>n;
-    return val;
+function add(a){ 
+     let res = function (b){ 
+   return add(a+b);
+    }
+   res.valueOf = function(){
+      return a;
+    }
+      return res;
   }
+  
